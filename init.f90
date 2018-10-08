5979bf803dcedbdc03330f9f7d89dcac40dba9c7
Ethan Payne <epay1@student.monash.edu>
2018-10-08 22:13:14 +1100

    Added data and columns and evsplash to github

--- init.f90
+++ init.f90
@@ -15,8 +15,11 @@ contains
     m(1) = 1.0
     m(2) = 1.0
     mtot = m(1) + m(2)
-    e = 0.7
-    a = 1.0
+    !e = 0.7
+    !a = 1.0
+    print*,'Enter the eccentricity and semi-major axis: '
+    read*,e,a
+
     r = a*(1+e)
     v0 = sqrt(a*(1-e**2)*mtot)/r
 
